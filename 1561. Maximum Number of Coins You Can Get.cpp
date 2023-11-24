b9406c5f965736cde10c905d473a7b83beaa80a3
Sourav Deb <33459970+debsourav33@users.noreply.github.com>
2023-11-23 18:04:37 -0700

    Create 1561. Maximum Number of Coins You Can Get.cpp

---
+++ 1561. Maximum Number of Coins You Can Get.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        int n = piles.size();
+        sort(piles.begin(),piles.end());
+
+        int ans = 0;
+        int collected = 0;
+
+        //leave first quarter (n/3) for Bob
+        //for the last 2 quarters, start from the last (largest pile):
+        //allocate it to Alice, you take the pile before it (immediate smaller)
+        //repeat until you reach Bob's quarter
+        for(int i=n-2;i>=n/3;i-=2)  ans += piles[i];
+
+        return ans;
+    }
+};
