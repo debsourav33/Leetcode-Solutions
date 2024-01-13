7fd38bcab9d66716b79f9ea8e873308210d5e162
Sourav Deb <33459970+debsourav33@users.noreply.github.com>
2024-01-12 19:24:42 -0700

    Backtracking, Bitmask

---
+++ 1239. Maximum Length of a Concatenated String with Unique Characters.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> v;
+    int maxi, n;
+
+    //convert a string to bitmask (one set bit for each character in string)
+    int get_mask(string &s){
+      int mask = 0;
+
+      for(char c: s){
+        int pos = c - 'a';
+
+        //check if duplicate characters present
+        if(mask & (1<<pos))  return -1;
+
+        mask |= (1<<pos);
+      }
+
+      return mask;
+    }
+
+    int get1count(int mask){
+      int cnt = 0;
+        //find no. of 1 bits (= no. of unique characters)
+        for(int i=0;i<26;i++){
+          if(mask & (1<<i))  cnt++;
+        }
+      return cnt;
+    }
+
+    void call(int pos, int mask){
+      for(int i=pos;i<n;i++){
+        //mask & v[i] > 0 means atleast 1 same character foind in v[i]
+        if(mask & v[i])  continue;
+
+        call(i+1, mask | v[i]); //if not, take all the characters of v[i], set bit for them and proceed
+      }
+
+      int cnt = get1count(mask);
+      maxi = max(cnt, maxi);
+    }
+
+    
+    int maxLength(vector<string>& arr) {
+        maxi = 0;
+        v.clear();
+        
+        for(string &s: arr){
+          int mask = get_mask(s);
+          if(mask!=-1)  v.push_back(mask);  //mask = -1 means duplicate characters in same string
+        }
+
+        n = v.size();
+
+        call(0,0);
+
+        return maxi;
+    }
+};
