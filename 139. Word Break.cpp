6816974d5b077b5772b1e5a8ebaee440f5d5c376
Sourav Deb <33459970+debsourav33@users.noreply.github.com>
2024-03-15 21:08:50 -0700

    DP

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -1,35 +1,52 @@
+/*
+Time: O(n*k*k) (k is the biggest length of word in dictionary)
+Space: O(n)
+
+Use DP
+At every index we have the option to 1. continue building the substring, or 2. take this substring (if it exists in dictionary) and continue a new substring from next index
+*/
+
 class Solution {
 public:
     unordered_map<string,bool> m;
 
-    vector<vector<int>> dp;
+    vector<int> dp;
     string s;
     int n;
 
-    //dp states - st : starting index of current substring, curr = ending index of current substring
-    bool call(int st, int curr, string t){
-      if(curr==n)  return m.count(t);
-      
-      int &mem = dp[st][curr];
-      if(mem!=-1)  return mem;
+    int max_len = 0;
 
-      bool ans = call(st,curr+1,t+s[curr]);
+    //dp states - st : starting index of current substring (all previous indices are guaranteed to be consumed)
+    bool call(int st){
+      if(st == n)  return true;
+      if(dp[st] != -1)  return dp[st];
 
-      if(m.count(t))  ans = ans || call(curr,curr+1, string(1,s[curr]));
+      string curr = "";
+      bool possible = false;
 
-      return mem = ans;
+      for(int i=st;i<n;i++){
+        if(i-st > max_len)  break; //substring length crossed the biggest word in dictionary
+
+        curr += s[i];
+        if(m.count(curr))  possible = possible || call(i+1); //m.count(curr) is o(k) operation if curr's length is k
+
+        if(possible)  return dp[st] = possible;
+      }
+
+      return dp[st] = false;
     }
 
     bool wordBreak(string s, vector<string>& words) {
         this->s = s;
         n = s.length();
 
-        dp = vector(n, vector(n,-1));
+        dp = vector(n, -1);
         
-        m.clear();
-        for(auto word: words)  m[word] = true;
+        for(auto word: words){
+            max_len = max(max_len, (int)word.length());
+            m[word] = true;
+        }
 
-
-        return call(0,0,"");
+        return call(0);
     }
 };
